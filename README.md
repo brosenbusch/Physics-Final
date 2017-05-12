6ab91511bdd223633204839c1b2d21c355294789
brosenbusch <8brosenbusch@stpatrick.org>
2017-05-12 13:55:20 -0500

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# Physics-Final
+# Fysics-Phinal
+
+This project involves making a program which will become a physics equation solver.  If you ask the right questions and give it the correct inputs, it will solve basic physics equations for you.
+
+Stay tuned as the instructions will change by Monday.
---
+++ physics.js
@@ -0,0 +1,21 @@
+function distance(v,t){
+  return v*t;
+}
+function velocity(d,t){
+  return d/t;
+}
+function time(d,v){
+  return d/v;
+}
+function velocityFinal(vi,a,t){
+  return vi+a*t;
+}
+function acceleration(v,t){
+  return v/t;
+}
+function force(m,a){
+  return m*a;
+}
+function forceG(G,m1,m2,r){
+  return (G*m1*m2)/(r*r);
+}
