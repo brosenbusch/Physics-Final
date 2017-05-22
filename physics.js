5670e07d5962a84695e399df72acf8384b7846a0
brosenbusch <8brosenbusch@stpatrick.org>
2017-05-22 13:04:49 -0500

    Add files via upload

--- physics.js
+++ physics.js
@@ -29,49 +29,50 @@ function convertToArray(strcma){
 
 terminal.question("What type of equation are you looking for?",function(str){
     if(str=="motion"){
-        terminal.question("Which variable are you solving for?",function(str1){
-            if(str1=="d"){
+        terminal.question("Which variable are you solving for?",function(str){
+            if(str=="d"){
                 printOptions();
                 terminal.question("What do you know?",function(str){
+                    str = convertToArray(str);
                     if(Number.isNaN(str[2])){
-                        convertToArray(str);
                         console.log(motion.distance1(str[1],str[3]));
                         end();
-
                     }
                     else{
-                        convertToArray(str);
                         console.log(motion.distance2(str[1],str[2],str[3]));
                         end();
                     }
                 });
-
             }
-
-            else if(str1=="v"){
+            else if(str=="v"){
                 printOptions();
                 terminal.question("What do you know?",function(str){
+                    str = convertToArray(str);
                     if(Number.isNaN(str[2])){
-                        convertToArray(str);
                         console.log(motion.velocity1(str[0],str[3]));
                         end();
                     }
                     else{
-                        convertToArray(str);
                         console.log(motion.velocity2(str[1],str[2],str[3]));
                         end();
                     }
                 });
             }
-            else if(str1=="t"){
-                convertToArray(str);
-                console.log(motion.time(str[0],str[1]));
-                end();
+            else if(str=="t"){
+                printOptions();
+                terminal.question("What do you know?",function(str){
+                    str = convertToArray(str);
+                    console.log(motion.time(str[0],str[1]));
+                    end();
+                });
             }
-            else if(str1=="a"){
-                convertToArray(str);
-                console.log(motion.acceleration(str[1],str[3]));
-                end();
+            else if(str=="a"){
+                printOptions();
+                terminal.question("What do you know?",function(str){
+                    str = convertToArray(str);
+                    console.log(motion.acceleration(str[1],str[3]));
+                    end();
+                });
             }
         });
     }
@@ -79,26 +80,30 @@ terminal.question("What type of equation are you looking for?",function(str){
         terminal.question("Which variable are you solving for?",function(str){
             printOptions();
             if(str=="ke"){
-                convertToArray(str);
-                console.log(energy.kineticEnergy(str[4],str[1]))
-                end();
+                printOptions();
+                terminal.question("What do you know?",function(str){
+                    str = convertToArray(str);
+                    console.log(energy.kineticEnergy(str[4],str[1]))
+                    end();
+                });
             }
             else if(str=="m"){
-                convertToArray(str);
-                console.log(energy.mass(str[5],str[1]))
-                end();
+                printOptions();
+                terminal.question("What do you know?",function(str){
+                    str = convertToArray(str);
+                    console.log(energy.mass(str[5],str[1]))
+                    end();
+                });
             }
             else if(str=="v"){
-                convertToArray(str);
-                console.log(energy.velocity(str[5],str[4]))
-                end();
+                printOptions();
+                terminal.question("What do you know?",function(str){
+                    str = convertToArray(str);
+                    console.log(energy.velocity(str[5],str[4]))
+                    end();
+                });
             }
         });
-
-
-
     }
-    //you will configure your series of questions inside here.
-
-
 });
+    //you will configure your series of questions inside here.
